aa73e0cb2c064484a9b1a808afb92899af203833
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-16 06:11:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -2,3 +2,6 @@
 first_number = 1
 second_nmber = 1
 sum = first_number + second_nmber
+difference = first_number - second_nmber
+product = first_number - second_nmber
+quotient = first_number / second_nmber
