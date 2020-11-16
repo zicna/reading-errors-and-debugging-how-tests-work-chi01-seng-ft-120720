82ebb1f0bbe9ddb529e7c18c5e98fadc58bd5dfb
beatific-queue-4949 <zivkovicmilan1987@gmail.com>
2020-11-16 06:12:19 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,7 +1,7 @@
 # Add your variables here
 first_number = 1
-second_nmber = 1
-sum = first_number + second_nmber
-difference = first_number - second_nmber
-product = first_number - second_nmber
-quotient = first_number / second_nmber
+second_number = 1
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number - second_number
+quotient = first_number / second_number
